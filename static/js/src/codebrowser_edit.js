066c9a3a2a4ce411bcf517888a30dcdd8940e08e
rainymoon911 <zhangrain911@163.com>
2015-08-12 11:44:00 +0800

    Update codebrowser_edit.js

--- static/js/src/codebrowser_edit.js
+++ static/js/src/codebrowser_edit.js
@@ -1,16 +1,23 @@
 function CodeBrowserEditBlock(runtime, element) {
-  $(element).find('.save-button').bind('click', function() {
-    var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
-    var data = {
-      width: $(element).find('input[name=width]').val(),
-      height: $(element).find('input[name=height]').val(),
-    };
-    $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
-      window.location.reload(false);
+  var generateHandlerUrl = runtime.handlerUrl(element, 'generate');
+  function jsCallback(response) {
+        if (response.result == true) {
+            window.location.reload(true);
+        } else {
+            $('.error-message', element).html('Error: ' + response.message);
+        }
+    }
+    
+  $('#generate_btn', element).click(function(eventObject) {
+        params = {
+            "lab": $("#lab", element).val()
+        };
+        $.ajax({
+            type: "POST",
+            url: generateHandlerUrl,
+            data: JSON.stringify(params),
+            success: jsCallback
+        });
+        $('.error-message', element).html();
     });
-  });
-
-  $(element).find('.cancel-button').bind('click', function() {
-    runtime.notify('cancel', {});
-  });
 }
