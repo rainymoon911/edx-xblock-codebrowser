fb7e53e9a610ce40d2dd55ee93799925c60b3b5c
rainymoon911 <zhangrain911@163.com>
2015-08-12 14:08:15 +0800

    Update codebrowser.py

--- codebrowser.py
+++ codebrowser.py
@@ -68,7 +68,7 @@ class CodeBrowserBlock(XBlock):
 		    file_config　=　open(config_file,'wa')
 		    file_config.write(config)
 		    file_config.close()
-		    os.system("chmod 600 “　＋　rsa_file)
+		    os.system("chmod 600 ”＋ rsa_file)
 		    ＃create code dir
 		    dir = "/edx/var/edxapp/staticfiles/ucore/" + student_id + "/ucore_lab"
 		    os.system("mkdir -p " + dir +  " && cd " + dir + " && git init")
